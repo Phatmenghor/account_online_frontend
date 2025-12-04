00d79219af42186e5ee661983b9d2ef66ca76f7a
Makara <makarakara79@gmail.com>
2025-12-04 10:28:51 +0700

    merge code

--- git_pull.sh
+++ git_pull.sh
@@ -5,6 +5,6 @@ CURRENT_TIME=$(date "+%Y-%m-%d %H:%M:%S")
 # Commit with date-time message
 git fetch
 # Push to main branch
-git pull origin development
+git pull origin test
 
-echo "✅ Code pushed to 'development' branch at $CURRENT_TIME"
+echo "✅ Code pushed to 'test' branch at $CURRENT_TIME"
